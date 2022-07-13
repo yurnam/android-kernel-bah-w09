3e2495dc37f236173fb03ac68d26d511299d44c6
yurnam <m.kritzer@gmx.at>
2022-07-13 10:37:34 +0200

    ...

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Wed Jul 13 10:34:39 CEST 2022"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "martin-GREED-PC"
+#define LINUX_COMPILER "gcc version 11.2.0 (Ubuntu 11.2.0-17ubuntu1) "
